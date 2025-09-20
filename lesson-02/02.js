1ef6ed6c69363d135682b124eb8f05c1744bef29
Ilya Rafeenko <ilyarafeenko12@icloud.com>
2025-09-21 01:01:32 +0300

    First commit lesson2-02

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,16 +7,23 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1; // your code
+let dice2; // your code
+let isWinningDouble; // your code
 
-/*
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
-if (isWinningDouble) {
-  console.log('Выигрышный дубль!')
+dice1 = Math.floor(Math.random() * 6) + 1;
+dice2 = Math.floor(Math.random() * 6) + 1;
+
+if (dice1 === dice2 && dice1 > 3 && dice2 > 3) {
+  isWinningDouble = true;
 } else {
-  console.log('Не выигрышный дубль.')
+  isWinningDouble = false;
 }
-*/
+
+console.log("Первый бросок: " + dice1);
+console.log("Второй бросок: " + dice2);
+if (isWinningDouble) {
+  console.log("Выигрышный дубль!");
+} else {
+  console.log("Не выигрышный дубль.");
+}
